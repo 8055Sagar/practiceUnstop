b5d9a30d32f0f30f5eb69d21bf0aa7befe7e4b09
Sagar <Sagarrajpoot029@gmail.com>
2023-07-23 21:22:10 +0530

    Updated images

--- src/desktop/desktop.js
+++ src/desktop/desktop.js
@@ -1,5 +1,5 @@
 import '../App.css';
-import dashboard from '../images/Dashboard.png';
+import dashboard_icon from '../images/Dashboard_icon.png';
 import roundStatus from '../images/admin_roundStatus.png';
 import assessment from '../images/assessment.png';
 import library from '../images/my_library.png';
@@ -9,7 +9,7 @@ function Desktop() {
   return (
         <div className='sideBar'>
           <div className=''>
-            <img src={dashboard} alt='dashboard' style={{cursor: 'pointer'}}></img>
+            <img src={dashboard_icon} alt='dashboard' style={{cursor: 'pointer'}}></img>
             <img src={assessment} alt='assessment' style={{cursor: 'pointer'}}></img>
             <img src={library} alt='my library' style={{cursor: 'pointer'}}></img>
             <img src={roundStatus} alt='roundStatus' style={{cursor: 'pointer'}}></img>
